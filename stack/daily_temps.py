52d4bbc092c9431e342da37d2e5243beddc07a9f
Matthew Kim <matthewkimcs@gmail.com>
2023-10-03 15:25:49 -0700

    car fleet rip my brain

---
+++ stack/daily_temps.py
@@ -0,0 +1,19 @@
+from typing import List
+
+
+class Solution:
+    def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+        res = [0] * len(temperatures)
+        stack = []  # pair: [temp, index]
+
+        for i, temperature in enumerate(temperatures):
+            while stack and temperature > stack[-1][0]:
+                stackT, stackIdx = stack.pop()
+                res[stackIdx] = (i - stackIdx)
+            stack.append([temperature, i])
+        return res
+
+
+app = Solution()
+
+app.dailyTemperatures([73, 74, 75, 71, 69, 72, 76, 73])
