dac470290504bd6d7752669895deb13b5c80df09
Matthew <matthewkimcs@gmail.com>
2024-02-04 12:40:55 -0800

    comments

--- stack/valid_parentheses.py
+++ stack/valid_parentheses.py
@@ -1,10 +1,13 @@
 class Solution:
     def isValid(self, s: str) -> bool:
+
         Map = {")": "(", "]": "[", "}": "{"}
         stack = []
 
         for c in s:
             if c not in Map:
+                # we know that if c is not in Map, it's a opening bracket. The "in" checks if the key is in a dict or not
+                # not the value
                 stack.append(c)
                 continue
             if not stack or stack[-1] != Map[c]:
