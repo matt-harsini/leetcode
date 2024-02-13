39bf0bced48c0f58924fd3de12b0615c92315020
Matthew Kim <matthewkim@Matthews-MBP-627.lan>
2024-02-12 18:51:32 -0800

    some notes for car fleet

--- stack/carfleet.py
+++ stack/carfleet.py
@@ -2,7 +2,7 @@ from typing import List
 
 class Solution:
     def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
-                
+              
         pair = [(p, s) for p, s in zip(position, speed)]
         pair.sort(reverse=True)
         stack = []
--- stack/daily_temps.py
+++ stack/daily_temps.py
@@ -3,6 +3,7 @@ from typing import List
 
 class Solution:
     def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+
         res = [0] * len(temperatures)
         stack = []  # pair: [temp, index]
 
